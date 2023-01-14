33027fb7bd9a810c32f0e1ad773696fff9e168f8
creatornicole <ngottschall@web.de>
2023-01-14 11:51:43 +0100

    adjusted view names to resource naming conventions

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,13 @@ Route::get('/', [ListingController::class, 'index']);
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-
+//Common Resource Routes:
+//(listing could be any kind of resource)
+//index - show all listings - show whole content
+//show - show single listing - show single part of content
+//create - show form to create new listing - display form on page to create new content
+//store - store new listing - submit form and store new content
+//edit - show form to edit listing - show edit form on page
+//update - update listing - to actually update content (submit edit form to update)
+//destroy - delete listing - to delete content
 
