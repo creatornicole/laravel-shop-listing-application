462d511083cbfd52a787f1a5732b991a4090fc03
creatornicole <ngottschall@web.de>
2023-01-15 10:27:41 +0100

    keep form data on input error

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -22,6 +22,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value="{{old('company')}}"
                             />
 
                             @error('company')
@@ -39,6 +40,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value="{{old('title')}}"
                             />
 
                             @error('title')
@@ -58,6 +60,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
+                                value="{{old('location')}}"
                             />
 
                             @error('location')
@@ -74,6 +77,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value="{{old('email')}}"
                             />
 
                             @error('email')
@@ -93,6 +97,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value="{{old('website')}}"
                             />
 
                             @error('website')
@@ -110,6 +115,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                                value="{{old('tags')}}"
                             />
 
                             @error('tags')
@@ -141,7 +147,7 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
-                            ></textarea>
+                            >{{old('description')}}</textarea>
 
                             @error('description')
                                 <!-- what do you if validation fails for this input -->
