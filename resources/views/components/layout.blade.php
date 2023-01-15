f5ed7867ffed88e6335afd55ae81a123d9676e9c
creatornicole <ngottschall@web.de>
2023-01-15 10:22:23 +0100

    implemented alpine js for timeout flash message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script src="//unpkg.com/alpinejs" defer></script> <!-- Alpine js -->
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
